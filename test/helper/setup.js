ebb672c84014dba52a26fb9bbcfa84179210972c
sescobb27 <s.escob.b.27@gmail.com>
2016-05-24 20:10:39 -0500

    [add] sinon.js for testing mocks

--- test/helper/setup.js
+++ test/helper/setup.js
@@ -3,8 +3,12 @@
  */
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
 
+chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
 GLOBAL.AssertionError = chai.AssertionError;
 GLOBAL.expect = chai.expect;
+GLOBAL.sinon = sinon;
